c3e2ea85306d7af0a03370e7ebcb3f23e8303691
jhenao-nex <jaime.andres.henao.arbelaez@ba.com>
2025-09-13 19:00:52 -0500

    feat: fix minikube script

--- .devcontainer/fix-minikube-perms.sh
+++ .devcontainer/fix-minikube-perms.sh
@@ -6,19 +6,29 @@ KUBE_DIR="$HOME/.kube"
 KUBECONFIG_FILE="$KUBE_DIR/config"
 KUBECONFIG_LOCK_FILE="$KUBE_DIR/config.lock"
 
+
 # Fix directory permissions
 if [ -d "$KUBE_DIR" ]; then
-  sudo chown -R $USER:$USER "$KUBE_DIR"
+  echo "Fixing permissions for $KUBE_DIR"
+  sudo chown -R "$USER:$USER" "$KUBE_DIR"
   sudo chmod 700 "$KUBE_DIR"
+else
+  echo "$KUBE_DIR does not exist, skipping directory permissions."
 fi
 
 # Fix file permissions
 if [ -f "$KUBECONFIG_FILE" ]; then
-  sudo chown $USER "$KUBECONFIG_FILE"
+  echo "Fixing permissions for $KUBECONFIG_FILE"
+  sudo chown "$USER:$USER" "$KUBECONFIG_FILE"
   sudo chmod 600 "$KUBECONFIG_FILE"
+else
+  echo "$KUBECONFIG_FILE does not exist, skipping file permissions."
 fi
 
 if [ -f "$KUBECONFIG_LOCK_FILE" ]; then
-  sudo chown $USER "$KUBECONFIG_LOCK_FILE"
+  echo "Fixing permissions for $KUBECONFIG_LOCK_FILE"
+  sudo chown "$USER:$USER" "$KUBECONFIG_LOCK_FILE"
   sudo chmod 600 "$KUBECONFIG_LOCK_FILE"
+else
+  echo "$KUBECONFIG_LOCK_FILE does not exist, skipping lock file permissions."
 fi
